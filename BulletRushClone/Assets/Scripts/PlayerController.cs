2d6e53f3bc648b60c77963212b0092490115021a
BilgeKaanpc <kashikoi.dev@gmail.com>
2022-07-04 11:25:16 +0300

    Player Move

--- BulletRushClone/Assets/Scripts/PlayerController.cs
+++ BulletRushClone/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public void Move()
-    {
+    [SerializeField] ScreenTouchController input;
+    [SerializeField] private Rigidbody myRigidbody;
+    [SerializeField] private float moveSpeed;
 
+    public void Move(Vector3 direction)
+    {
+        myRigidbody.velocity = direction * moveSpeed * Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        var direction = new Vector3(input.Direction.x, 0, input.Direction.y);
+        Move(direction);
     }
 }
--- BulletRushClone/Assets/Scripts/ScreenTouchController.cs
+++ BulletRushClone/Assets/Scripts/ScreenTouchController.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ScreenTouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+
+    private Vector2 _touchPosition;
+    public Vector2 Direction { get; private set; }
+
+    [SerializeField] private Image pivotImage;
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("dokundu");
+        _touchPosition = eventData.position;
+        pivotImage.enabled = true;
+        pivotImage.transform.position = _touchPosition;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("kaldırdı");
+        Direction = Vector2.zero;
+        pivotImage.enabled = false;
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-        Debug.Log("sürükledi");
+
+       var delta= eventData.position - _touchPosition;
+        Direction = delta.normalized;
+
     }
 }
