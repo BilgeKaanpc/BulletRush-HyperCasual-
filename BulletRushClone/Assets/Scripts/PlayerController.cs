64d2d030614ca9893b34055bf6734a035534c9db
BilgeKaanpc <kashikoi.dev@gmail.com>
2022-07-04 11:02:02 +0300

    Mobil Controller

---
+++ BulletRushClone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public void Move()
+    {
+
+    }
+}
---
+++ BulletRushClone/Assets/Scripts/ScreenTouchController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScreenTouchController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("dokundu");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("kaldırdı");
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("sürükledi");
+    }
+}
